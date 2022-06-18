7bafe49178f18c67edc505a06eea7db9e89201d4
ayush-mathpal <92756482+AyushMathpal@users.noreply.github.com>
2022-06-18 16:38:56 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function Decrement1(){
+  const value=document.getElementById("no-of-items-1");
+  const output=parseInt(value.innerText)-1;
+value.innerText=output;
+if(output<0){value.innerText=0;}
+}
+function Increment1(){
+  const value=document.getElementById("no-of-items-1");
+  const output=parseInt(value.innerText)+1;
+value.innerText=output;
+}
+function Decrement2(){
+  const value=document.getElementById("no-of-items-2");
+  const output=parseInt(value.innerText)-1;
+value.innerText=output;
+if(output<0){value.innerText=0;}
+}
+function Increment2(){
+  const value=document.getElementById("no-of-items-2");
+  const output=parseInt(value.innerText)+1;
+value.innerText=output;
+if(output<0){value.innerText=0;}
+}
+function Alert(){
+  alert("Data Validation Successfull");
+
+}
+function ValidateEmail(event, inputText){
+   event.preventDefault();
+}
